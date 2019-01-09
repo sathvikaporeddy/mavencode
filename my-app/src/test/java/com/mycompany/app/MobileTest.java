5d3dddae224da9cf2af684ecaa33fd8b92f03be9
SamhithaPoreddy <samhitha.poreddy@utah.edu>
2019-01-09 16:34:22 -0700

    mav code my app

---
+++ my-app/src/test/java/com/mycompany/app/MobileTest.java
@@ -0,0 +1,15 @@
+package com.mycompany.app;
+
+import org.junit.Test;
+
+public class MobileTest {
+	
+		@Test
+		public void websignin() {
+			System.out.println("websignin");
+		}
+		@Test
+		public void websignout() {
+			System.out.println("websignout");
+		}
+	}
---
+++ my-app/src/test/java/com/mycompany/app/WebTest.java
@@ -0,0 +1,14 @@
+package com.mycompany.app;
+
+import org.junit.Test;
+
+public class WebTest {
+	@Test
+	public void websignin() {
+		System.out.println("websignin");
+	}
+	@Test
+	public void websignout() {
+		System.out.println("websignout");
+	}
+}
